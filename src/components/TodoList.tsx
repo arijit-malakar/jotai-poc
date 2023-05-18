e8bd8be3318f1249f80753008432dbd85d2755c0
Arijit Malakar <arijitmalakar@Arijits-MacBook-Air.local>
2023-05-18 17:53:09 +0530

    added completed todos feature and tabs for filtering lists.

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -11,6 +11,7 @@ type Todo = {
 type Todos = Todo[];
 
 const todosAtom = atom<Todos>([]);
+const completedTodosAtom = atom<Todos>([]);
 
 const addTodoAtom = atom(null, (get, set, text: string) => {
     const todos = get(todosAtom);
@@ -24,12 +25,22 @@ const addTodoAtom = atom(null, (get, set, text: string) => {
 
 const toggleTodoAtom = atom(null, (get, set, id: number) => {
     const todos = get(todosAtom);
-    const updatedTodos = todos.map((todo) => {
+    const completedTodos = get(completedTodosAtom);
+    const updatedTodos = todos?.map((todo) => {
         if (todo.id === id) {
-            return {
+            const updatedTodo = {
                 ...todo,
-                completed: !todo.completed
+                completed: !todo.completed,
             };
+            if (updatedTodo.completed) {
+                set(completedTodosAtom, [...completedTodos, updatedTodo]);
+            } else {
+                set(
+                    completedTodosAtom,
+                    completedTodos?.filter((t) => t.id !== updatedTodo.id)
+                );
+            }
+            return updatedTodo;
         }
         return todo;
     });
@@ -38,19 +49,21 @@ const toggleTodoAtom = atom(null, (get, set, id: number) => {
 
 const clearCheckedTodo = atom(null, (get, set) => {
     const todos = get(todosAtom);
-    const updatedTodos = todos.filter((todo) => !todo.completed);
+    const updatedTodos = todos?.filter((todo) => !todo.completed);
     set(todosAtom, updatedTodos);
 })
 
 const TodoList: React.FC = () => {
     const [text, setText] = useState('');
+    const [activeTab, setActiveTab] = useState('All');
     const [todos] = useAtom(todosAtom);
+    const [completedTodos] = useAtom(completedTodosAtom);
     const [, addTodo] = useAtom(addTodoAtom);
     const [, toggleTodo] = useAtom(toggleTodoAtom);
     const [, clearChecked] = useAtom(clearCheckedTodo);
 
     const handleAddTodo = () => {
-        if (text.trim()) {
+        if (text?.trim()) {
             addTodo(text);
             setText('');
         }
@@ -64,47 +77,79 @@ const TodoList: React.FC = () => {
         clearChecked();
     };
 
-    const renderedTodos = todos.map((todo) => (
+    const handleTabClick = (tab: string) => {
+        setActiveTab(tab);
+    }
+
+    let filteredTodos = todos;
+    if (activeTab === 'Completed') {
+        filteredTodos = completedTodos;
+    }
+
+    const renderedTodos = filteredTodos?.map((todo) => (
         <div key={todo.id} className="todo-list-item mb-3">
-            <label className="checkbox">
-                <input className="mr-3"
-                    type="checkbox"
-                    checked={todo.completed}
-                    onChange={() => handleToggleTodo(todo.id)}
-                />
+            {activeTab === 'Completed' ? (
                 <span>{todo.text}</span>
-            </label>
+            ) : (
+                <label className="checkbox">
+                    <input className="mr-3"
+                        type="checkbox"
+                        checked={todo.completed}
+                        onChange={() => handleToggleTodo(todo.id)}
+                    />
+                    <span>{todo.text}</span>
+                </label>
+            )}
         </div>
     ));
 
     return (
         <section className="section m-auto">
             <div className="columns is-centered m-auto">
-                    <div className="panel is-success w-40">
-                        <h3 className="panel-heading py-4">To-do List</h3>
-                        <div className="todo-add">
-                                <div className="field is-grouped">
-                                    <div className="control is-expanded">
-                                        <input
-                                            className="input is-success"
-                                            placeholder="New Task"
-                                            value={text}
-                                            autoComplete="off"
-                                            onChange={(e) => setText(e.target.value)} />
-                                    </div>
-                                    <div className="control">
-                                        <button className="button is-primary" onClick={handleAddTodo}>Add</button>
-                                    </div>
-                                </div>
+                <div className="panel is-success w-40">
+                    <h3 className="panel-heading py-4">To-do List</h3>
+                    <div className="todo-add">
+                        <div className="field is-grouped">
+                            <div className="control is-expanded">
+                                <input
+                                    className="input is-success"
+                                    placeholder="New Task"
+                                    value={text}
+                                    autoComplete="off"
+                                    onChange={(e) => setText(e.target.value)} />
+                            </div>
+                            <div className="control">
+                                <button className="button is-primary" onClick={handleAddTodo}>Add</button>
+                            </div>
+                        </div>
+                        <div className="tabs">
+                            <ul>
+                                <li
+                                    className={activeTab === 'All' ? 'is-active' : ''}
+                                    onClick={() => handleTabClick('All')}
+                                >
+                                    <a href="/#" onClick={(e) => e.preventDefault()}>All</a>
+                                </li>
+                                <li
+                                    className={activeTab === 'Completed' ? 'is-active' : ''}
+                                    onClick={() => handleTabClick('Completed')}
+                                >
+                                    <a href="/#" onClick={(e) => e.preventDefault()}>Completed</a>
+                                </li>
+                            </ul>
+                        </div>
                         <div className="mt-5">
-                            {todos.length > 0 ? renderedTodos : <div className="todo-box-empty">Seems like you haven't got any tasks!</div>}
-                        </div>                            
-                        <div className="panel-block p-0 mt-5">
-                            <button className="button is-danger is-outlined is-fullwidth" onClick={handleClearCompleted}>Clear Completed</button>
+                            {filteredTodos?.length > 0 ? renderedTodos : <div className="todo-box-empty">Seems like you haven't got any tasks!</div>}
                         </div>
-                        </div>
+                        {activeTab === 'All' ? (
+                            <div className="panel-block p-0 mt-5">
+                                <button className="button is-danger is-outlined is-fullwidth" onClick={handleClearCompleted}>Clear Completed</button>
+                            </div>
+                        ) : ''}
+
                     </div>
-          
+                </div>
+
             </div>
         </section>
     );
